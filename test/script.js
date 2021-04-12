737b3f9d05846ea415b498e719f423203fe58ce5
AGonz123 <32293916+AGonz123@users.noreply.github.com>
2021-04-12 13:11:12 -0700

    Update script.js

--- test/script.js
+++ test/script.js
@@ -9,3 +9,19 @@ $icon.on('click', function(){
   }
 });
 
+var Form1 = document.getElementById("Form1");
+    var Form2 = document.getElementById("Form2");
+
+    var Next = document.getElementById("Next");
+    var Back = document.getElementById("Back");
+
+    Next.onclick = function(){
+      Form1.style.left = "-450px";
+      Form2.style.left = "40px";
+    }
+
+    Back.onclick = function(){
+      Form1.style.left = "40px";
+      Form2.style.left = "450px";
+    }
+
